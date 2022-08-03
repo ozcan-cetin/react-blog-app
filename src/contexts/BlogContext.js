cd58aa298ab05d7d10f11899079bc448b1c5469b
özcan çetin <cetin445@gmail.com>
2022-08-03 15:55:05 +0300

    blog context children

--- src/contexts/BlogContext.js
+++ src/contexts/BlogContext.js
@@ -1,4 +1,7 @@
+import { ref, update } from "firebase/database";
 import React, { createContext, useContext, useState } from "react";
+import { db } from "../helpers/firebase";
+import { toastErrorNotify } from "../helpers/toastNotify";
 import { AuthContext } from "./AuthContext";
 
 export const BlogContext = createContext();
@@ -17,12 +20,51 @@ const BlogContextProvider = ({ children }) => {
     usersId: [""]
   };
 
- 
+   const [blog, setBlog] = useState(initialValues);
 
-  const [blog, setBlog] = useState(initialValues);
+   const increaseLike = (blog) => {
+    // console.log(blog.usersId);
+    if(currentUser){
+      if(!Object.values(blog.usersId).includes(currentUser.email)){
+        console.log(Object.values(blog.usersId));
+        // console.log(currentUser);
+        // const db = getDatabase(app);
+      const updates={}
+      updates["blogs/"+blog.id]={
+          ...blog,
+          like: blog.like + 1,
+          usersId:[...blog.usersId,currentUser.email]
+        }
+        setColor(true)
+      return update(ref(db),updates)
+      }
+      else{
+        // toastErrorNotify("You have already clicked")
+        const updates={}
+        updates["blogs/"+blog.id]={
+            ...blog,
+            like: blog.like - 1,
+            // usersId:[...blog.usersId,blog.id]
+            usersId:[(blog.usersId).filter((item)=>item!==currentUser.email)]
+          }
+          setColor(false)
+          return update(ref(db),updates)
+      }
+    }
+    else{
+      toastErrorNotify("Please login first")
+    }
+      
+      }
+
+      const editBlog = (blog) => {
+        // setBlog({ id, title, url, content, userName: currentUser.displayName });
+        setBlog({...blog, id:blog.id, title:blog.title, url:blog.url, content:blog.content});
+        // setBlog(blog);
+      };
 
   return (
-    <BlogContext.Provider value={{ blog, setBlog, initialValues }}>
+    <BlogContext.Provider value={{ blog, setBlog, initialValues, editBlog, increaseLike, color }}>
       {children}
     </BlogContext.Provider>
   );
